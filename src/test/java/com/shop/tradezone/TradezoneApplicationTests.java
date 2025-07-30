3451573bd6baf4e1f0b57a44414277665bec9f36
yunsjak <yunsujung0819@naver.com>
2025-07-30 12:39:55 +0900

    TradezoneApplicationTests.java add

---
+++ src/test/java/com/shop/tradezone/TradezoneApplicationTests.java
@@ -0,0 +1,17 @@
+package com.shop.tradezone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TradezoneApplicationTests {
+
+	@Autowired
+
+	@Test
+	void contextLoads() {
+
+	}
+
+}
